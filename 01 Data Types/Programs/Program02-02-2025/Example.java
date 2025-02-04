6a656897aebe174a9257a4780ed1b36fd2135e07
Rambali5550 <114878301+Arvindlodhi2001@users.noreply.github.com>
2025-02-04 12:31:13 +0530

    Method and Types of Methods

---
+++ 01 Data Types/Programs/Program02-02-2025/Example.java
@@ -0,0 +1,18 @@
+
+class Example1{
+	                 int Sum(int A , int B){
+		                  return A+B;
+	                          }
+                    }
+
+public class Example{
+	            
+	
+                public static void main(String[] args){
+				      System.out.println("Hello Arvind Lodhi");
+					  
+					  Example1 E1 = new Example1();
+					  int c = E1.Sum(12,24);
+					  System.out.println("Total Is --->" + c);
+				}         
+    }
